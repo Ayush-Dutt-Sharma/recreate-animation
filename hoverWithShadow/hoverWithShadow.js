26e89a0ecca633f1938d71719cf36dc322b08435
Ayush Dutt Sharma <duttsharmarahul17@gmail.com>
2024-06-28 17:05:43 +0530

    Create hoverWithShadow.js

---
+++ hoverWithShadow/hoverWithShadow.js
@@ -0,0 +1,13 @@
+import './hoverWithShadow.css'
+const App =()=>{
+return <>
+  <div className="list">
+  <div className="item"><img width="100px" height="150px"src="./temp-death-note-01.webp"/></div>
+  <div className="item"><img width="100px" height="150px"src="./temp-goku-01.jpg"/></div>
+  <div className="item"><img width="100px" height="150px"src="./temp-jujutsu-01.jpg"/></div>
+  <div className="item"><img width="100px" height="150px"src="./temp-demon-01.jpg"/></div>
+    <div className="item"><img width="100px" height="150px"src="./temp-naruto-01.jpg"/></div>
+  </div>
+</>
+}
+export default App;
